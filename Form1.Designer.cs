f8b4b87ce8292232d8b62d1c3af19cdf8305f492
Gardenius <tomeksolyszko@gmail.com>
2018-10-26 19:06:47 +0200

    Signed-off-by: Gardenius <tomeksolyszko@gmail.com>

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Base64
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Koduj = new System.Windows.Forms.Button();
+            this.dekoduj = new System.Windows.Forms.Button();
+            this.Input_Button = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Input_Text = new System.Windows.Forms.Label();
+            this.Output_Button = new System.Windows.Forms.Button();
+            this.Output_Text = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // Koduj
+            // 
+            this.Koduj.Location = new System.Drawing.Point(12, 12);
+            this.Koduj.Name = "Koduj";
+            this.Koduj.Size = new System.Drawing.Size(75, 23);
+            this.Koduj.TabIndex = 1;
+            this.Koduj.Text = "Koduj";
+            this.Koduj.UseVisualStyleBackColor = true;
+            this.Koduj.Click += new System.EventHandler(this.Koduj_Click);
+            // 
+            // dekoduj
+            // 
+            this.dekoduj.Location = new System.Drawing.Point(12, 41);
+            this.dekoduj.Name = "dekoduj";
+            this.dekoduj.Size = new System.Drawing.Size(75, 23);
+            this.dekoduj.TabIndex = 1;
+            this.dekoduj.Text = "Dekoduj";
+            this.dekoduj.UseVisualStyleBackColor = true;
+            this.dekoduj.Click += new System.EventHandler(this.dekoduj_Click);
+            // 
+            // Input_Button
+            // 
+            this.Input_Button.Location = new System.Drawing.Point(93, 12);
+            this.Input_Button.Name = "Input_Button";
+            this.Input_Button.Size = new System.Drawing.Size(25, 23);
+            this.Input_Button.TabIndex = 2;
+            this.Input_Button.Text = "...";
+            this.Input_Button.UseVisualStyleBackColor = true;
+            this.Input_Button.Click += new System.EventHandler(this.Input_Button_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Input_Text
+            // 
+            this.Input_Text.AutoSize = true;
+            this.Input_Text.Location = new System.Drawing.Point(124, 17);
+            this.Input_Text.Name = "Input_Text";
+            this.Input_Text.Size = new System.Drawing.Size(29, 13);
+            this.Input_Text.TabIndex = 3;
+            this.Input_Text.Text = "Path";
+            // 
+            // Output_Button
+            // 
+            this.Output_Button.Location = new System.Drawing.Point(93, 41);
+            this.Output_Button.Name = "Output_Button";
+            this.Output_Button.Size = new System.Drawing.Size(25, 23);
+            this.Output_Button.TabIndex = 2;
+            this.Output_Button.Text = "...";
+            this.Output_Button.UseVisualStyleBackColor = true;
+            this.Output_Button.Click += new System.EventHandler(this.Output_Button_Click);
+            // 
+            // Output_Text
+            // 
+            this.Output_Text.AutoSize = true;
+            this.Output_Text.Location = new System.Drawing.Point(124, 46);
+            this.Output_Text.Name = "Output_Text";
+            this.Output_Text.Size = new System.Drawing.Size(29, 13);
+            this.Output_Text.TabIndex = 3;
+            this.Output_Text.Text = "Path";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 70);
+            this.Controls.Add(this.Output_Text);
+            this.Controls.Add(this.Input_Text);
+            this.Controls.Add(this.Output_Button);
+            this.Controls.Add(this.Input_Button);
+            this.Controls.Add(this.dekoduj);
+            this.Controls.Add(this.Koduj);
+            this.Name = "Form1";
+            this.Text = "Base64";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Koduj;
+        private System.Windows.Forms.Button dekoduj;
+        private System.Windows.Forms.Button Input_Button;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label Input_Text;
+        private System.Windows.Forms.Button Output_Button;
+        private System.Windows.Forms.Label Output_Text;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Base64
+{
+    public partial class Form1 : Form
+    {
+        
+        public string b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+        public Form1()
+        {
+            InitializeComponent();
+            Input_Text.Text = "Wybierz plik";
+            Output_Text.Text = "Wybierz folder zapisu";
+        }
+        
+        private void Koduj_Click(object sender, EventArgs e)
+        {
+            if (Input_Text.Text == "Wybierz plik")
+            {
+                MessageBox.Show("Wybierz plik!");
+            }
+            else if (Output_Text.Text == "Wybierz folder zapisu")
+            {
+                MessageBox.Show("Wybierz folder zapisu!");
+            }
+            else
+            {
+                Kodowanie();
+                MessageBox.Show("Ukończono kodowanie.");
+            }
+            
+        }
+
+        private void dekoduj_Click(object sender, EventArgs e)
+        {
+            if (Input_Text.Text == "Wybierz plik")
+            {
+                MessageBox.Show("Wybierz plik!");
+            }
+            else if (Output_Text.Text == "Wybierz folder zapisu")
+            {
+                MessageBox.Show("Wybierz folder zapisu!");
+            }
+            else
+            {
+                Dekodowanie();
+                MessageBox.Show("Ukończono dekodowanie.");
+            }
+            
+        }
+
+        private void Kodowanie()
+        {
+            string input = Input_Text.Text;
+            string output = Output_Text.Text;
+            File.CreateText(Output_Text.Text + "\\CODED_FILE").Close();
+            byte[] fileBytes = File.ReadAllBytes(input);
+            StringBuilder sb = new StringBuilder();
+            int Counter = 0;
+            string i = ""; //Padding 1
+            int d = 0;
+            string base64 = "";
+
+            foreach (byte b in fileBytes)
+            {
+                sb.Append(Convert.ToString(b, 2).PadLeft(8, '0'));
+            }
+
+            int x = sb.ToString().Length;
+            foreach (char b in sb.ToString())
+            {
+                i += b;
+                Counter++;
+
+                if (x % 6 == 4 && Counter == x) i += "00";
+                else if (x % 6 == 2 && Counter == x) i += "0000";
+
+                if (i.Length % 6 == 0)
+                {
+                    d = Convert.ToInt16((i), 2);
+                    i = "";
+                    base64 += b64[d];
+                    if (base64.Length == 2000)
+                    {
+                        using (StreamWriter sw = File.AppendText(output + "\\CODED_FILE"))
+                        {
+                            sw.Write(base64);
+                            base64 = "";
+                        }
+                    }
+                }
+
+            }
+
+            if (x % 6 == 4) base64 += "=";
+            if (x % 6 == 2) base64 += "==";
+            using (StreamWriter sw = File.AppendText(output + "\\CODED_FILE"))
+            {
+                sw.Write(base64);
+                base64 = "";
+            }
+        }
+        private void Dekodowanie()
+        {
+            File.CreateText(Output_Text.Text + "\\DECODED_FILE").Close();
+            string input = Input_Text.Text;
+            string output = Output_Text.Text;
+            string tekst = File.ReadAllText(input);
+            string x = "";
+            string y = "";
+            int numOfBytes = 0;
+            byte[] bytes = new byte[numOfBytes];
+
+            foreach (char b in tekst)
+            {
+                x += Convert.ToString((b64.IndexOf(b)), 2).PadLeft(6, '0');
+                if (x.Length == 2400)
+                {
+                    y = x.ToString();
+                    numOfBytes = y.Length / 8;
+
+                    bytes = new byte[numOfBytes];
+                    for (int i = 0; i < numOfBytes; ++i)
+                    {
+                        bytes[i] = Convert.ToByte(y.Substring(8 * i, 8), 2);
+                    }
+
+                    using (var stream = new FileStream(output + "\\DECODED_FILE", FileMode.Append))
+                    {
+                        stream.Write(bytes, 0, bytes.Length);
+                        x = "";
+
+                    }
+                    
+                }
+
+
+            }
+            y = x.ToString();
+            numOfBytes = y.Length / 8;
+            bytes = new byte[numOfBytes];
+            for (int i = 0; i < numOfBytes; ++i)
+            {
+                bytes[i] = Convert.ToByte(y.Substring(8 * i, 8), 2);
+            }
+
+            using (var stream = new FileStream(output + "\\DECODED_FILE", FileMode.Append))
+            {
+                stream.Write(bytes, 0, bytes.Length);
+                x = "";
+            }
+
+
+
+
+        }
+        private void Input_Button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                String filename = openFileDialog1.FileName;
+                Input_Text.Text = filename;
+            }
+        }
+        private void Output_Button_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                String path = folderBrowserDialog1.SelectedPath;
+                Output_Text.Text = path;
+            }
+        }
+
+    }
+}
